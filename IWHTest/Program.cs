cab5da8cda5bf87194116e6bebac7d3a4c1cf70a
Sergey Sumarokov <sumarokov@nm.ru>
2016-03-10 20:17:18 +0300

    #ADD IWHTEst умеет читать node из xml

---
+++ IWHTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWHTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (XmlReader xml = XmlReader.Create("ExampleOfOSM.xml"))
+            {
+                while (xml.Read())
+                {
+
+                    if (xml.NodeType == XmlNodeType.Element)
+                    {
+                        if (xml.Name == "node")
+                        {
+                            XmlDocument xmlDoc = new XmlDocument();
+                            xmlDoc.LoadXml(xml.ReadOuterXml());
+                            XmlNode n = xmlDoc.SelectSingleNode("node");
+                            Console.WriteLine("ID: {0}", n.Attributes["id"].Value);
+                            foreach (XmlNode t in xmlDoc.SelectNodes("/node/tag"))
+                            {
+                                Console.WriteLine("k: {0}", t.Attributes["k"].Value);
+                            }
+                            Console.WriteLine("------------------------------------------");
+                        }
+                    }
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
