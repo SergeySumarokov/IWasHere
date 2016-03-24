99b00fa4ad19c54d0ff4e671d4fdc1b40dd79198
Sergey Sumarokov <sumarokov@nm.ru>
2016-03-24 16:44:44 +0300

    ADD# Root

---
+++ IWHLibrary/Root/Node.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Primitives;
+
+namespace IWH
+{
+
+    public enum NodeType : int
+    {
+        Waypoint = 0,
+        PopulatedPlace = 1,
+        ShowPlace = 2
+    }
+
+    public class Node : OSM.Node
+    {
+
+        public NodeType Type { get; set; }
+
+        /// <summary>
+        /// Радиус окуржности, при входе в которую точка считается посещенной.
+        /// </summary>
+        public Distance Range;
+
+        /// <summary>
+        /// Длина участка пути (удаление от предыдущей точки линии).
+        /// </summary>
+        public Distance PartLenght;
+
+        /// <summary>
+        /// Направление участка пути (азимут от предыдущей точки линии).
+        /// </summary>
+        public Angle PartDirection;
+
+        /// <summary>
+        /// Истина, исли точка была посещена.
+        /// </summary>
+        public Boolean IsVisited;
+
+        /// <summary>
+        /// Время последнего посещения точки.
+        /// </summary>
+        public DateTime LastVisitedTime;
+
+    }
+
+}
---
+++ IWHLibrary/Root/Way.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Primitives;
+
+namespace IWH
+{
+
+    public enum WayType : int
+    {
+        Unknown = 0,
+        Motorway = 1,
+        Trunk = 2,
+        Primary = 3,
+        Secondary = 4
+    }
+
+    public class Way :OSM.Way
+    {
+
+        /// <summary>
+        /// Тип линии.
+        /// </summary>
+        public WayType Type { get; set; }
+
+        /// <summary>
+        /// Общая протяженность линии.
+        /// </summary>
+        public Distance Lenght;
+
+        /// <summary>
+        /// Суммарная протяженность посещённых участков линии.
+        /// </summary>
+        public Distance VisitedLenght;
+
+        /// <summary>
+        /// Истина, исли вся линия была посещена.
+        /// </summary>
+        public Boolean IsVisited;
+
+        /// <summary>
+        /// Время последнего посещения любой из точек линии.
+        /// </summary>
+        public DateTime LastVisitedTime;
+
+        public void Recalculate()
+        {
+            foreach (var n in Nodes)
+            {
+
+            }
+        }
+
+    }
+
+}
