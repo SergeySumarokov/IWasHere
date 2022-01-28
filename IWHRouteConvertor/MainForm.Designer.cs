62c2ebade4eab4609a31bf657c3c089d9207e190
SergeySumarokov <sersum73@mail.ru>
2022-01-28 09:00:03 +0300

    Добавлен RouteConvertor

---
+++ IWHRouteConvertor/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace IWHRouteConvertor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(71, 67);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(371, 464);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выгрузить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Формат";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(545, 309);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(545, 379);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Обратный порядок";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 609);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "MainForm";
+            this.Text = "RouteConvertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ IWHRouteConvertor/Route.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Primitives;
+using Geography;
+
+namespace IWHRouteConvertor
+{
+    public enum RouteFormat : int
+    {
+        Native = 0,
+        YandexURL = 1,
+        GoogleURL = 2,
+        RTE = 3,
+        KML = 4
+    }
+    
+    /// <summary>
+    /// Представляет точку маршрута/
+    /// </summary>
+    public class RoutePoint : GeoPoint
+    {
+        /// <summary>
+        /// Имя точки маршрута.
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// Значение Истина указывает на промежуточную точку маршрута
+        /// </summary>
+        public Boolean Hidden;
+    }
+
+    /// <summary>
+    /// Представляет маршрут, заданный набором промежуточных точек.
+    /// </summary>
+    public class Route
+    {
+       
+        /// <summary>
+        /// Упорядоченный список точек, описывающий маршрут.
+        /// </summary>
+        public List<RoutePoint> Points { get; private set; }
+        
+        /// <summary>
+        /// Загружает маршрут из текстового представления заданного формата
+        /// </summary>
+        public void Read(RouteFormat routeFormat)
+        {
+            
+        }
+        
+        public void Write()
+        {
+
+        }
+
+    }
+}
